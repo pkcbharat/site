d7da524df446a31165460b897b531be2f153f480
PriyanshuCK <priyanshu.sharma.rampura@gmail.com>
2022-01-01 11:51:00 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!--Coding Just for Fun...
+PKC Bharat
+Priyanshu Sharma -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Let's come together and create awareness among parents and teachers to teach the children good deeds and form their demeanor.">
+    <meta name="theme-color" :content="darkMode ? '#1f2937' : '#3b82f6'" content="#3b82f6">
+    <title> PKC Bharat </title>
+    <link rel="shortcut icon" href="src/img/favicon.png" type="image/x-icon">
+</head>
+
+<body>
+    <?php
+
+    echo "Hello Azure!";
+    ?>
+</body>
+
+</html>
