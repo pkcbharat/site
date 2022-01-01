1a93f5933140410ee7004c96e836138454da6f52
PriyanshuCK <priyanshu.sharma.rampura@gmail.com>
2022-01-01 12:06:01 +0530

    Updated Favicon url

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ Priyanshu Sharma -->
     <meta name="description" content="Let's come together and create awareness among parents and teachers to teach the children good deeds and form their demeanor.">
     <meta name="theme-color" :content="darkMode ? '#1f2937' : '#3b82f6'" content="#3b82f6">
     <title> PKC Bharat </title>
-    <link rel="shortcut icon" href="src/img/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="https://pkc.scm.azurewebsites.net/wwwroot/src/img/favicon.png" type="image/x-icon">
 </head>
 
 <body>
